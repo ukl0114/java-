c4bd88e9134e423a3b76a4bac84a770a9c3854c0
ukl0114 <ukl0114@naver.com>
2022-11-27 15:25:42 +0900

    두번째

---
+++ java_test/src/test1/codingTest2/test1.java
@@ -0,0 +1,17 @@
+package test1.codingTest2;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int apple = 5;
+		int pear = 6;
+		int orange = 7;
+		int total = apple + pear + orange;
+		float one_day = total / 24f;
+		
+		System.out.println("과수원에서 하루에생산되는 과일 총 개수 : " + total);
+		System.out.println("시간당 생산량 : " + one_day);
+	}
+
+}
